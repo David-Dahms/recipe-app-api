b543d702553f716a90a5cde4784998449a6ec32e
David-Dahms <djdahms24@gmail.com>
2022-09-14 10:40:01 -0400

    Code Checker 2

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,11 +4,11 @@ Calculator functions
 """
 
 
-def add(x,y):
-    """Add x and y return result."""
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
 
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,7 +4,7 @@ Django command to wait for the database to be available.
 """
 import time
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -19,9 +19,9 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['defaut'])
+                self.check(databases=['default'])
                 db_up = True
-            except (Psycopg20pError, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
