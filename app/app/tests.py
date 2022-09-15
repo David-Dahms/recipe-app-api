5fe36ad9ee76eb2242c3b25f2941e2616420989c
David-Dahms <djdahms24@gmail.com>
2022-09-15 17:28:08 -0400

    user model

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,6 +8,7 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
+
     """Test the calc module."""
 
     def test_add_numbers(self):
@@ -15,7 +16,6 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
