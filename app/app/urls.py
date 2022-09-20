89403f99a7d9b3ef18813cc5a2e7237bfd18d254
David-Dahms <djdahms24@gmail.com>
2022-09-20 14:20:07 -0400

    rest framework

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.view import (
+from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
@@ -27,7 +27,7 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
-        ),
-        path('api/user/', include('user.urls')),
+        name='api-docs',
+    ),
+    path('api/user/', include('user.urls')),
 ]
